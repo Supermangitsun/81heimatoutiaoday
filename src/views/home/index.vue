187e7d79007f1aecfa14656c1b7e9f46e68a3c1d
Supermangitsun <635600284@qq.com>
2019-08-20 14:11:46 +0800

    主页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>Home</div>
+  <!-- 最外层容器 -->
+  <el-container>
+    <el-aside>
+      左侧内容
+    </el-aside>
+<!-- 嵌套一个容器container -->
+  <el-container>
+  <!-- 头部区域 -->
+      <el-header>
+        头部内容
+      </el-header>
+      <!-- 中间区域 -->
+      <el-main>
+        中间区域
+        </el-main>
+  </el-container>
+  </el-container>
 </template>
 
 <script>
